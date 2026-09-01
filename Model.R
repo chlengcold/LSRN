f960e43fced27801c115dc3baab75da9ff3e3177
Rin Leng <64882208+chlengcold@users.noreply.github.com>
2026-09-01 10:07:54 +0800

    Add files via upload

--- Model.R
+++ Model.R
@@ -1,18 +1,18 @@
 # Defining model
-run_MCMC_allcode = function(data, constants, inits, thin = 40, niter = 60000, nburnin = 20000, calc_dist=F) {
+run_MCMC_allcode = function(data, constants, inits, thin = 40, niter = 60000, nburnin = 20000) {
   library(nimbleHMC)
-  
+
   code = nimbleCode({ 
     ##### Model #####
     for(i in 1:N) {    
       for(j in J_idx[i,]){
-        
         for(p in 1:P){
-          d[ i, j, 1, p] <- 1		
+          dist[i,j,p] <- sqrt(sum((xi[i,1:V,item_dim[p]] - xi[j,1:V,item_dim[p]])^2))
+          eta[i,j,p] <- theta_S[i,item_dim[p]] + theta_R[j,item_dim[p]] - beta[p] - lambda[delta[p]+1,p] * dist[i,j,p]
+          
+          d[ i, j, 1, p] <- 1	
           for(k in 2:K){
-            d[i, j, k, p] <- exp((k-1)*(theta_S[i, item_dim[p]] + theta_R[j, item_dim[p]] - beta[p] 
-                                        - lambda[delta[p]+1, p] * sqrt(sum((xi[i, 1:V, item_dim[p]] - xi[j, 1:V, item_dim[p]])^2)) ) 
-                                 - sum(tau[1:(k-1),item_dim[p]]) ) 
+            d[i,j,k,p] <- exp((k-1)*eta[i,j,p] - tau_sum[k, item_dim[p]])
           }
           
           cd[i, j, p] <- sum(d[ i, j, 1:K, p]) 
@@ -24,19 +24,24 @@ run_MCMC_allcode = function(data, constants, inits, thin = 40, niter = 60000, nb
       } 
     }   
     
+    for(l in 1:L){
+      tau_sum[1,l] <- 0
+      for(k in 2:K){
+        tau_sum[k,l] <- sum(tau[1:(k-1),l])
+      }
+    }
+    
     # Priors 
     for(p in 1:P){
       ## Beta
       beta_raw[p] ~ dnorm(0, var=4)
       
-      ## lambda_ind
+      ## lambda
       log_lambda[1, p] ~ dnorm(-8,1)
       log_lambda[2, p] ~ dnorm(0.5,1)
       lambda[1:2, p] <- exp(log_lambda[1:2, p])
-      omega[p] ~ dbeta(1,1)
-      delta[p] ~ dbern(omega[p]) 
+      delta[p] ~ dbern(0.5) 
     }
-    # beta[1:P] <- beta_raw[1:P] - mean(beta_raw[1:P])
     
     for(l in 1:L){
       beta[(item_seq[2*l-1]):(item_seq[2*l])] <- beta_raw[(item_seq[2*l-1]):(item_seq[2*l])] - mean(beta_raw[(item_seq[2*l-1]):(item_seq[2*l])])
@@ -47,28 +52,18 @@ run_MCMC_allcode = function(data, constants, inits, thin = 40, niter = 60000, nb
       }
       tau[1:(K-1), l] <- tau_raw[1:(K-1), l] - mean(tau_raw[1:(K-1), l])    
     }
- 
+    
     ## rho
     Ustar[,] ~ dlkj_corr_cholesky(1,2*L) # upper-triangular
     Sigma[1:(2*L),1:(2*L)] <- t(Ustar[1:(2*L),1:(2*L)]) %*% Ustar[1:(2*L),1:(2*L)]
     
     for(i in 1: N){
-      theta[i, 1:(2*L)] ~ dmnorm(M[1:(2*L)],  cholesky = Ustar[1:(2*L), 1:(2*L)], prec_param = 0)
-
-      logL_raw[i, i, 1:K, 1:P] <- zero_KP[1:K,1:P]
+      theta[i, 1:(2*L)] ~ dmnorm(M[1:(2*L)],  cov = Sigma[1:(2*L),1:(2*L)])
       
       for(l in 1:L){
         xi_raw[i,1:V,l] ~ dmnorm(M_xi[1:V], cov = Sigma_xi[1:V, 1:V])
       }
-      
-      for(p in 1:P){
-        for(j in J_idx[i,]){
-          for(k in 1:K){
-            logL_raw[i, j, k, p] <- log(prob[i, j, k, p]) * (y[i, j, p] == k)
-          } # k
-        } # j
-      } # p
-    } # i
+    } 
     
     theta_S[1:N, 1:L] <- theta[1:N, 1:L]
     theta_R[1:N, 1:L] <- theta[1:N, (L+1):(2*L)]
@@ -80,25 +75,18 @@ run_MCMC_allcode = function(data, constants, inits, thin = 40, niter = 60000, nb
     }
     
   })
-  Rmodel <- nimbleModel(code,  data = dta, inits = inits, constants = constants, buildDerivs = TRUE)
-  
-  ## create default MCMC configuration object 
-  monitors = c('theta', 'xi', 'omega', 'lambda', 'beta', 'tau', 'dist_ij', 'Sigma', 'logL_raw')
-  if(calc_dist==T){
-    conf <- configureMCMC(Rmodel, enableWAIC = F,
-                          monitors = monitors) 
-  }else{
-    conf <- configureMCMC(Rmodel, enableWAIC = F,
-                          monitors = monitors[-grep('dist', monitors)]) 
-  }
+  Rmodel <- nimbleModel(code,  data = data, inits = inits, constants = constants, buildDerivs = TRUE)
 
-  ## add an HMC sampler operating on b0 and b1 
-  addHMC(conf, target = c('theta', 'log_lambda', 'omega', 'xi_raw', 'Ustar', 'beta_raw', 'tau_raw'), replace=T) 
+  monitors = c('theta', 'xi', 'delta', 'lambda', 'beta', 'tau', 'Sigma')
+  conf <- configureMCMC(Rmodel, enableWAIC = F, monitors = monitors) 
+
+  addHMC(conf, target = c('theta', 'log_lambda', 'xi_raw', 'Ustar', 'beta_raw', 'tau_raw'), replace=T) 
   Rmcmc <- buildMCMC(conf)
   
   Cmodel <- compileNimble(Rmodel) 
   Cmcmc <- compileNimble(Rmcmc, project = Rmodel) 
-  samples <- runMCMC(Cmcmc, thin = 1, niter = 100, nburnin = 50, nchains = 1,
+  
+  samples <- runMCMC(Cmcmc, thin = thin, niter = niter, nburnin = nburnin, nchains = 1,
                      summary = T, WAIC = F, samplesAsCodaMCMC=T)
   
   return(samples)
